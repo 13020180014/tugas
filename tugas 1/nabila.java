e9a47ca1ba7bdc72249aaed5ec2e1e4bae8d87d8
13020180014 <61999864+13020180014@users.noreply.github.com>
2020-03-10 14:53:18 +0700

    Add files via upload

---
+++ tugas 1/nabila.java
@@ -0,0 +1,6 @@
+public class Belajar{
+       public static void main(String[] args){
+       System.out.println("Belajar Bahasa Pemograman Java");
+       }
+
+}
---
+++ tugas 1/pemograman.java
@@ -0,0 +1,6 @@
+public class pemograman\ {
+       public static void main(String[] args){
+       System.out.println(" Selesai Belajar Pemograman Java");
+       }
+
+}
